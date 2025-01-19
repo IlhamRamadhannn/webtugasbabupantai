a327e874e1578957e7943fe57cc258aa2ed4c104
IlhamRamadhannn <ilhamsr05@gmail.com>
2025-01-19 13:21:24 +0700

    as

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     @include('custom.bootstrap5')
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <style >
         p{
